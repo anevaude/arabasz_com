e4cb7fcd92f9a42ab973d1e128ed2530da0401d6
Annette Arabasz <aarabasz@madpow.net>
2016-09-23 16:01:48 -0400

    Updating README and adding core files

---
+++ Gruntfile.js
@@ -0,0 +1,74 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    sass: {
+      options: {
+        require: 'susy',
+        sourcemap: 'auto',
+        style: 'nested',
+        loadPath: [
+          '/Users/aarabasz/.rvm/gems/ruby-2.1.5/gems/susy-2.2.2/sass'
+        ]
+      },
+      compile: {
+        files: {
+          'app/assets/css/styles.css': 'app/assets/scss/styles.scss'
+        }
+      }            
+    },        
+    nodeunit: {
+      files: ['test/**/*_test.js'],
+    },   
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc'
+      },
+      gruntfile: {
+        src: 'Gruntfile.js'
+      },
+      lib: {
+        src: ['lib/**/*.js']
+      },
+      test: {
+        src: ['test/**/*.js']
+      },
+    },
+    watch: {
+      gruntfile: {
+        files: '<%= jshint.gruntfile.src %>',
+        tasks: ['jshint:gruntfile']
+      },
+      sass: {
+          files: 'app/**/*.{scss,sass}',
+          tasks: ['sass']
+      },
+      lib: {
+        files: '<%= jshint.lib.src %>',
+        tasks: ['jshint:lib', 'nodeunit']
+      },      
+      test: {
+        files: '<%= jshint.test.src %>',
+        tasks: ['jshint:test', 'nodeunit']
+      },
+    },
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-nodeunit');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-serve');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-sass');    
+
+  // Default task.
+  grunt.registerTask('default', [
+    'nodeunit',
+    'jshint',
+    'watch',
+    'sass'    
+  ]);
+};
--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # arabasz.com
 
-This is the code for my portfolio website, http://arabasz.com
+This is the code for my portfolio website, http://arabasz.com.
 
 ## What's Inside?
-I am using these nifty tools:
+I am using these nifty tools. Thanks to the authors for making these open source!
 * Sass (http://sass-lang.com)
 * Susy (http://susy.oddbird.net)
 * Grunt (http://gruntjs.com)
